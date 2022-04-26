0bf2b821b2e3cc76f78f3c6dc1a0a3cbf0105c80
Arnaud GIRAUD <nono@MacBook-Pro-de-Arnaud.local>
2022-04-26 12:46:11 +0200

    Updates

---
+++ precache-manifest.2fa781e5017593b1b4a6ba1eb65fe4f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc88674e97e46666dbc4add36f0a1c42",
+    "url": "/Alyra3/index.html"
+  },
+  {
+    "revision": "1415b2ee62ed7b43de0e",
+    "url": "/Alyra3/static/css/2.c7d41ee2.chunk.css"
+  },
+  {
+    "revision": "b7178ee6ed3ce730a121",
+    "url": "/Alyra3/static/css/main.6c554a89.chunk.css"
+  },
+  {
+    "revision": "1415b2ee62ed7b43de0e",
+    "url": "/Alyra3/static/js/2.bc6ea92e.chunk.js"
+  },
+  {
+    "revision": "b7178ee6ed3ce730a121",
+    "url": "/Alyra3/static/js/main.88655840.chunk.js"
+  },
+  {
+    "revision": "628ae8060acbac9898b8",
+    "url": "/Alyra3/static/js/runtime-main.06407f1a.js"
+  }
+]);
